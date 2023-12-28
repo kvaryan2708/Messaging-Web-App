e3df1003d401a89556ed74350553c83e8a3706b3
Aryan Kumar <kvaryan2708@gmail.com>
2023-12-29 00:30:09 +0530

    bhqd

--- api/server.js
+++ api/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb://127.0.0.1:27017/MsgApp', {
+mongoose.connect('mongodb+srv://kvaryan2708:aryan420@cluster1.hdx3pzq.mongodb.net/?retryWrites=true&w=majority', {
 	useNewUrlParser: true, 
 	useUnifiedTopology: true 
 }).then(() => console.log("Connected to MongoDB")).catch(console.error);
